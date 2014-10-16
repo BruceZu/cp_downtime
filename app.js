80abf355ef38d421cb63456ca0d8b40714b2b5f1
Alex V <alex.quez@gmail.com>
2014-10-15 21:04:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* globals require, console */
+var http = require("http");
+var fs   = require("fs");
+
+fs.readFile("./down.html", function(err, data) {
+  // console.log("Listening on port 8080");
+
+  "use strict";
+
+  http.createServer(function (req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  }).listen(8080);
+});
