437526fbc2df7ad3283138cefb61bd8081ad3b8f
Alex V <alex.quez@gmail.com>
2014-10-15 21:29:26 -0700

    responsd with json to posts

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var http = require("http");
 var fs   = require("fs");
 
 fs.readFile("./down.html", function(err, data) {
-  // console.log("Listening on port 8080");
-
   "use strict";
 
   http.createServer(function (req, res) {
     res.writeHeader(200, { "Content-Type": "text/html" });
-    res.write(data);
+
+    if (req.method === "POST") {
+      var msg = "CodePen is temporarily down for maintenance.";
+      var postResponse = { "success": false, "errors": [msg] };
+      res.write(JSON.stringify(postResponse));
+    } else {
+      res.write(data);
+    }
     res.end();
   }).listen(3000);
 });
