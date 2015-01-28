2a84a3c9af558ca5fdd2799ac8c6e7f66dea5c2a
Alex V <alex.quez@gmail.com>
2015-01-27 19:32:51 -0800

    Return 500 error for a few predefined URLs

--- app.js
+++ app.js
@@ -2,19 +2,42 @@
 var http = require("http");
 var fs   = require("fs");
 
+function handle200(req, res, data) {
+  res.writeHeader(200, { "Content-Type": "text/html" });
+
+  if (req.method === "POST") {
+    var msg = "CodePen is temporarily down for maintenance.";
+    var postResponse = { "success": false, "errors": [msg] };
+    res.write(JSON.stringify(postResponse));
+  } else {
+    res.write(data);
+  }
+
+  res.end();
+}
+
+function handle500(req, res) {
+  res.writeHeader(500, { "Content-Type": "text/html" });
+
+  var msg = "CodePen is temporarily down for maintenance.";
+  var postResponse = { "success": false, "errors": [msg] };
+
+  res.write(JSON.stringify(postResponse));
+  res.end();
+}
+
 fs.readFile("./down.html", function(err, data) {
   "use strict";
+  console.log("Starting server\n");
 
   http.createServer(function (req, res) {
-    res.writeHeader(200, { "Content-Type": "text/html" });
+    var url = req.url || "";
 
-    if (req.method === "POST") {
-      var msg = "CodePen is temporarily down for maintenance.";
-      var postResponse = { "success": false, "errors": [msg] };
-      res.write(JSON.stringify(postResponse));
+    if (url.match(/^(\/subscription\/stripe\/callback|\/comment\/spam\/mark\/queue|\/comment\/spam\/approve\/queue|\/admin\/worker\/run|\/oauth\/goog\/files\/delete-expired|\/pen\/add_pick_from_queue|\/pen\/screenshot\/update)/)) {
+      handle500(req, res);
     } else {
-      res.write(data);
+      handle200(req, res, data);
     }
-    res.end();
+
   }).listen(3000);
 });
